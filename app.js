c3b42b2888dedf5a147a2b8080a1b153715318eb
Connor <connor.alcock.87@gmail.com>
2017-07-27 11:22:11 -0600

    deployed client

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 // const express = require('express');
 // const app = express();
 // const route = app('/.routes.js');
-const knex = require('./db/knex.js');
+const knex = require('../groceryProject_SERVER/db/knex.js');
 
 knex('grocery').select('*').then(function(groceries) {
   console.log(groceries)
