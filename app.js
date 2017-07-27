0ff9bdd6fd60346005825d4e718949b0a9426593
Connor <connor.alcock.87@gmail.com>
2017-07-27 10:30:29 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+// const express = require('express');
+// const app = express();
+// const route = app('/.routes.js');
+const knex = require('./db/knex.js');
+
+knex('grocery').select('*').then(function(groceries) {
+  console.log(groceries)
+});
